d30097f585aa15c930d121ada26a361d01982876
jorchube <jorchube@debstation>
2018-02-18 15:07:54 +0100

    i3 custom session changes | added screen locker

--- i3wm/session_launcher.sh
+++ i3wm/session_launcher.sh
@@ -2,6 +2,9 @@
 
 /usr/bin/xfsettingsd &
 /usr/bin/compton &
+/usr/bin/nm-applet &
 
+sleep 1
 #This can not run in bg; thus has to be started last
-/usr/bin/i3 
+#/usr/bin/i3 
+/opt/bin/i3
--- metascripts/install_custom_i3_session.sh
+++ metascripts/install_custom_i3_session.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo ln -s ~/.rc/i3wm/i3-jorchube.desktop /usr/share/xsessions/i3-jorchube.desktop
+sudo ln -s ~/.rc/i3wm/i3-custom-session.desktop /usr/share/xsessions/i3-custom-session.desktop
+sudo ln -s ~/.rc/i3wm/i3-custom-session-lockscreen.desktop /usr/share/applications/i3-custom-session-lockscreen.desktop
