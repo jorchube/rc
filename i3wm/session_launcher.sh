6bd8a6aedbd47a6feb79c5a86edbc581e30f9ff5
Jordi Chulia <jordi.chulia@casa-systems.com>
2018-02-21 14:12:04 +0100

    add xrandr options in session launcher

--- i3wm/session_launcher.sh
+++ i3wm/session_launcher.sh
@@ -4,6 +4,8 @@ xrdb -merge ./.Xresources
 /usr/bin/compton &
 /usr/bin/nm-applet &
 
+#xrandr --output HDMI-2 --mode 1920x1080 --rate 60.00 --output DP-1 --mode 1920x1080 --rate 60.00 --right-of HDMI-2 --output eDP-1 --mode 1360x768 --rate 59.80 --left-of HDMI-2 
+
 sleep 1
 
 #This can not run in bg; thus has to be started last:
