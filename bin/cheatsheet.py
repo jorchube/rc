6d0ba65a898e0d82a1523f29118517a6162b9c1c
Jordi Chulia <jordi.chulia@casa-systems.com>
2018-02-21 15:57:41 +0100

    Added cheatsheet entries

--- bin/cheatsheet.py
+++ bin/cheatsheet.py
@@ -12,6 +12,27 @@ tmux_cp = \
     paste with Control+b ]
 """
 
+tmux_panes = \
+"""
+- panes:
+    split horizontally: Control+b \"
+    split vertically: Control+b %
+    move to pane: Control+b arrow_key
+    resize pane: Control+b Control+arrow_key
+    swap pane position to the left: Contorl+b {
+    swap pane position to the right: Contorl+b {
+    toggle pane fullscreen: Control+b z
+    kill pane: Contorl+b x
+"""
+
+tmux_windows = \
+"""
+- windows:
+    create window: Control+b c
+    switch to window: Contorl+b session_number
+    move window to other session: move-window -t target-session:
+"""
+
 tmux_buffers = \
 """
 - handle buffers:
@@ -32,23 +53,44 @@ tmux_session = \
     end session: Control+b :kill-session
 """
 
-tmux_window = \
-"""
-- windows:
-    move window to other session: move-window -t target-session:
-"""
 
-
-i3wm_moving_aroung = \
+i3wm_moving_around = \
 """
-- moving around:
+- launching apps:
     open terminal: Mod+Enter
-    
+    application launcher: Mod+d
+    close application: Mod+Shift+q
+    set split vertical: Mod+v
+    set split horizontal: Mod+h
+    move to window: Mod+arrow_key
+    move to workspace: Mod+workspace_number
+    exit i3wm session: Mod+Shift+e
 """
 
-tmux_tips   = [tmux_cp, tmux_buffers, tmux_session, tmux_window]
+i3wm_layout = \
+"""
+- layout:
+    toggle horizontal/vertical container layout: Mod+e
+    toggle tabbed mode: Mod+w
+    focus to parent window container mode node: Mod+a
+    toggle fullscreen: Mod+f
+    move window: Mod+Shift+arroy_key
+    move window to workspace: Mod+Shift+workspace_number
+"""
+
+i3wm_window = \
+"""
+- window:
+    move floating: Mod+mouse_left_click
+    resize floating: Mod+mouse_right_click
+    enter resize mode: Mod+r
+        resize: Mod+mouse_right_click
+    exit resize mode: Esc
+"""
+
+tmux_tips   = [tmux_cp, tmux_buffers, tmux_session, tmux_windows, tmux_panes]
 vim_tips    = []
-i3wm_tips   = []
+i3wm_tips   = [i3wm_moving_around, i3wm_layout, i3wm_window]
 
 component_tips_dict = {
         'tmux' : tmux_tips,
