7bd7b7d24331c58192f583d28607ea17a29adb80
Jordi Chulia <jordi.chulia@casa-systems.com>
2018-02-21 12:31:51 +0100

    Small fixes

--- metascripts/install_custom_i3_session.sh
+++ metascripts/install_custom_i3_session.sh
@@ -2,5 +2,8 @@
 
 cp "$HOME/.rc/i3wm/session_launcher.sh" "$HOME/.rc/local.conf.d/session_launcher.sh" 
 
+sudo rm /usr/share/xsessions/i3-custom-session.desktop 2> /dev/null
+sudo rm /usr/share/applications/i3-custom-session-lockscreen.desktop 2> /dev/null
+
 sudo ln -s ~/.rc/i3wm/i3-custom-session.desktop /usr/share/xsessions/i3-custom-session.desktop
 sudo ln -s ~/.rc/i3wm/i3-custom-session-lockscreen.desktop /usr/share/applications/i3-custom-session-lockscreen.desktop
