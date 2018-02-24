94b8dbdded0c6003a65e5de5caf5a334f545b061
jorchube <jorchube@debstation>
2018-02-24 01:03:18 +0100

    change atom for vscode in default installation packages

--- metascripts/install_packages.sh
+++ metascripts/install_packages.sh
@@ -2,20 +2,26 @@
 
 GAPS_FOLDER=gaps
 ATOM_PACKAGE=atom.deb
+VSCODE_PACKAGE=vscode.deb
 
 
 # repository packages and dependencies
 sudo apt update
-sudo apt install lightdm compton network-manager network-manager-gnome dunst pcmanfm mate-terminal x11-xserver-utils feh git libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev pasystray pulseaudio pulseaudio-utils pavucontrol pavumeter wget gconf2 gconf-service libgtk2.0-0 libudev1 libgcrypt20 libnotify4 libxtst6 libnss3 python gvfs-bin xdg-utils libcap2 gpicview file-roller i3status i3lock suckless-tools
+sudo apt install lightdm compton network-manager network-manager-gnome dunst pcmanfm mate-terminal x11-xserver-utils feh git libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev pasystray pulseaudio pulseaudio-utils pavucontrol pavumeter wget gconf2 gconf-service libgtk2.0-0 libudev1 libgcrypt20 libnotify4 libxtst6 libnss3 python gvfs-bin xdg-utils libcap2 gpicview file-roller i3status i3lock suckless-tools libnss3 gnupg libxkbfile1 libgconf-2-4 libsecret-1-0
 
 
 # external packages:
 cd /tmp
 
 # atom editor
-wget https://atom.io/download/deb -O $ATOM_PACKAGE
-sudo dpkg -i $ATOM_PACKAGE
-rm -rf $ATOM_PACKAGE
+#wget https://atom.io/download/deb -O $ATOM_PACKAGE
+#sudo dpkg -i $ATOM_PACKAGE
+#rm -rf $ATOM_PACKAGE
+
+# visual studio code editor
+wget https://go.microsoft.com/fwlink/?LinkID=760868 -O $VSCODE_PACKAGE
+sudo dpkg -i $VSCODE_PACKAGE
+rm -rf $VSCODE_PACKAGE
 
 # i3-gaps
 git clone https://github.com/Airblader/i3.git $GAPS_FOLDER
